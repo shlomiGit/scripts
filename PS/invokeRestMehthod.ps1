2e5ef035a734a166b1150e79c10dd61b6487c550
shlomiGit <mail_s@walla.com>
2018-12-19 12:57:16 +0200

    Create invokeRestMehthod.ps1
    this script invokes a REST method with basic auth or local creds

---
+++ PS/invokeRestMehthod.ps1
@@ -0,0 +1,17 @@
+params(
+[string] $url
+[string] $userName
+[string] $pwd
+[string] $method
+)
+
+###headers
+# AzureDevops enables PAT, which allows $userName="" and $pwd=pat
+$basicAuth = ("{0}:{1}" -f $userName,$pwd)
+$basicAuth = [System.Text.Encoding]::UTF8.GetBytes($basicAuth)
+$basicAuth = [System.Convert]::ToBase64String($basicAuth)
+$headers = @{Authorization=("Basic {0}" -f $basicAuth)}
+
+###invoke
+#$jsonResponse = Invoke-RestMethod -Uri $uri -UseDefaultCredentials -Method $method
+$jsonResponse = Invoke-RestMethod -Uri $uri -Headers $headers -Method $method
