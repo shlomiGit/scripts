588595c692e6ef3344f5b9624b0d40a30545034a
shlomiGit <mail_s@walla.com>
2018-12-19 12:35:06 +0200

    Create versionUpdate.ps1
    this script reads file content, searches or the version expression using regex, updates the forth digit and saves to the orig file

---
+++ PS/versionUpdate.ps1
@@ -0,0 +1,22 @@
+# Enable -Verbose option
+[CmdletBinding()]
+Param(
+  [string]$filePath #= "C:\\temp\\Servotronix\\temp\\version.cpp"
+)
+if ($filePath -and (Test-Path $filePath)) {
+	$fileContent = Get-Content $filePath
+	$regex = 'Version [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
+	$versionString = select-string -Path $filePath -Pattern $regex -AllMatches | % { $_.Matches } | % { $_.Value }
+	if ($versionString) {
+    	$versionArray = $versionString.Split('.')
+    	[int] $lastBuild = $versionArray[3]
+    	$lastBuild++
+    	$versionArray[3] = $lastBuild
+    	[String] $newVersionString = $versionArray -join '.'
+    	$newVersionString
+    	Write-Host "Replacing ""$versionString"" with ""$newVersionString"""
+    	$fileContent -replace $versionString,$newVersionString > "${filePath}"
+	}
+} else {
+	Write-Error "Cannot find filePath ${filePath}"
+}
