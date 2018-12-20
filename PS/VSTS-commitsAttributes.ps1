96f633f0461c4735348107604d15d34e09de4466
shlomiGit <mail_s@walla.com>
2018-12-20 12:33:00 +0200

    Create VSTS-commitsAttributes.ps1
    this scripts gets commits by specific filter and extracts the email attribute

---
+++ PS/VSTS-commitsAttributes.ps1
@@ -0,0 +1,30 @@
+###### get commits with comment=increase version and Author=Elimelech
+#uri
+$author = 'Elimelech'
+[uri] $uri = "https://dev.azure.com/servotronix/servotronix/_apis/git/repositories/Maxx_Firmware/commits?searchCriteria.author=$author&api-version=4.1"
+ 
+#headers
+$basicAuth = ("{0}:{1}" -f "eli.elimelech@servotronix.com","Amit1409$")
+$basicAuth = [System.Text.Encoding]::UTF8.GetBytes($basicAuth)
+$basicAuth = [System.Convert]::ToBase64String($basicAuth)
+$headers = @{Authorization=("Basic {0}" -f $basicAuth)}
+#Invoke-RestMethod -Uri $uri -UseDefaultCredentials -Method Get
+$jsonResponse = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
+###### extract previous to last increase version commit
+[int] $indexOfPrevLast = 1
+$prevLastCommit = ($jsonResponse.value | Where {$_.comment -eq "increase version"})[$indexOfPrevLast]
+$lastCommitId = $prevLastCommit.commitId
+###### get commits between last increase version and current
+#uri
+[string] $repo = 'Maxx_Firmware'
+[uri] $uri = "https://dev.azure.com/servotronix/servotronix/_apis/git/repositories/$repo/commits?api-version=4.1"
+#Invoke-RestMethod -Uri $uri -UseDefaultCredentials -Method Get
+$jsonResponse = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
+#index of last commit
+$lastCommit = $jsonResponse.value | Where {$_.commitId -eq $lastCommitId}
+[int] $indexOfLastIncreaseVersionCommit = $jsonResponse.value.IndexOf($lastCommit)
+#fetch all commits up to last commit Id
+$relevantArray = $jsonResponse.value[0..$indexOfLastIncreaseVersionCommit]
+#fetch unique emails from commits
+[string] $emails = $relevantArray[0..$relevantArray.Length].author.email | select -Unique
+$emails
