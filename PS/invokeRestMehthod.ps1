431d76c5a5d79a57801b6430a9a2e31d9cba3bf5
shlomiGit <mail_s@walla.com>
2019-02-25 12:55:03 +0200

    Update invokeRestMehthod.ps1

--- PS/invokeRestMehthod.ps1
+++ PS/invokeRestMehthod.ps1
@@ -2,7 +2,7 @@ params(
 [Parameter(Mandatory=$true)]
 [string] $url
 [string] $userName
-[string] $pwd
+[secureString] $pwd
 [string] $method
 )
 
@@ -12,6 +12,14 @@ $basicAuth = ("{0}:{1}" -f $userName,$pwd)
 $basicAuth = [System.Text.Encoding]::UTF8.GetBytes($basicAuth)
 $basicAuth = [System.Convert]::ToBase64String($basicAuth)
 $headers = @{Authorization=("Basic {0}" -f $basicAuth)}
+
+######### headers object #########
+$headersObj = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headersObj.Add("Authorization","Basic Z19zemVldmk6U2hsMTIzNDU2")
+$headersObj.Add("X-Atlassian-Token","nocheck")
+
+### creds ###
+[pscredential] $creds = New-Object pscredential($userName, $pwd)
 
 ###invoke
 #$jsonResponse = Invoke-RestMethod -Uri $uri -UseDefaultCredentials -Method $method
