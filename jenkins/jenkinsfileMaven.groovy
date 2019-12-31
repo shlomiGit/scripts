c12a084d6ca9ece08b856b1d01e39d7491eba8ff
shlomiGit <mail_s@walla.com>
2019-12-31 11:46:46 +0200

    Update jenkinsfileMaven.groovy

--- jenkins/jenkinsfileMaven.groovy
+++ jenkins/jenkinsfileMaven.groovy
@@ -1,16 +1,27 @@
-def call () {
+def call (deploy_to_dev_var) {
+	def srq_name
+	def version
+	def deploy_to_env
 
 pipeline {
+	parameters {
+		booleanParam(defaultValue: false, description: '', name: 'deploy_to_dev')
+		booleanParam(defaultValue: false, description: '', name: 'deploy_to_test')
+		booleanParam defaultValue: false, description: 'force deploying a version that is already installed', name: 'force'
+	}
+	options{
+		buildDiscarder(logRotator(numToKeepStr: "${numberOfBuildsToKeep}"))
+	}
 	environment {
 		display = "${display}"
 		// vars for stage 'build'
 		mavenGoals = 'clean install -U -X'
-		dependencyReleaseRepo = '???????'
-		dependencySnapshotRepo = '???????'
-		deployReleaseRepo = '???????'
-		deploySnapshotRepo = '???????'
+		dependencyReleaseRepo = 'cal-maven-all'
+		dependencySnapshotRepo = 'cal-maven-all'
+		deployReleaseRepo = 'cal-internal-release'
+		deploySnapshotRepo = 'cal-internal-snapshots'
 		// vars for stage 'archive'
-		pattern = 'target/*.*ar'
+		pattern = '**/target/*.*ar, **/target/*.zip, **/pom.xml'
 	}
 	agent any
 	stages {
@@ -31,6 +42,30 @@ pipeline {
 			steps{
 				archiveArtifacts "${pattern}"
 			}
+		}
+		stage('deploy'){
+			when{
+				anyOf{
+					environment name: 'deploy_to_dev', value: 'true'; environment name: 'deploy_to_test', value: 'true'; equals expected: "true", actual: "${deploy_to_dev_var}"
+				}
+			}			
+		    steps{
+				script {
+					pom = readMavenPom file: 'pom.xml'
+					srq_name = pom['artifactId']
+					version = pom['version']
+					group = JOB_NAME.substring(0,JOB_NAME.indexOf('-'))
+					
+					if(deploy_to_dev_var==true || deploy_to_dev=='true'){
+						deploy_to_env = 'dev'
+						build job: "${group}-deploy", parameters: [string(name: 'srq_name', value: "${srq_name}"), string(name: 'version', value: "${version}"), string(name: 'deploy_to_env', value: "${deploy_to_env}"), string(name: 'force', value: "${force}")]				
+					}
+					if(deploy_to_test=='true'){
+						deploy_to_env = 'test'
+						build job: "${group}-deploy", parameters: [string(name: 'srq_name', value: "${srq_name}"), string(name: 'version', value: "${version}"), string(name: 'deploy_to_env', value: "${deploy_to_env}"), string(name: 'force', value: "${force}")]				
+					}
+				}		        
+		    }
 		}
 	}
 }
