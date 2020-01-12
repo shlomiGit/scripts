9c7f931ed4f287ff623a243ad66e907eb9bd3e57
shlomiGit <mail_s@walla.com>
2020-01-12 13:15:52 +0200

    add key
    in order to avoid computer dependancy

--- PS/secureString.ps1
+++ PS/secureString.ps1
@@ -2,10 +2,10 @@
 $filePath = 'c:\temp\encPswd.txt'
 
 #save pswd from user input to encripted file
-Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File $filePath
+Read-Host -AsSecureString | ConvertFrom-SecureString -Key (1..16) | Out-File $filePath
 
 #get pswd as secureString
-[SecureString] $securePassword = ConvertTo-SecureString (Get-Content $filePath)
+[SecureString] $securePassword = ConvertTo-SecureString -Key (1..16) (Get-Content $filePath)
 
 #PScredential object
 [pscredential] $ServiceAccountCreds = new-object -TypeName pscredential($username,$securePassword)
