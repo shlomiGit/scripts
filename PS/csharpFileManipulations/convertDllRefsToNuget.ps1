a2e4c0d5167d5b746c4920119a3815780cd01bc3
shlomiGit <mail_s@walla.com>
2020-03-31 12:35:21 +0300

    Update and rename PS/convertDllRefsToNuget.ps1 to PS/csharpFileManipulations/convertDllRefsToNuget.ps1

--- PS/csharpFileManipulations/convertDllRefsToNuget.ps1
+++ PS/csharpFileManipulations/convertDllRefsToNuget.ps1
@@ -5,17 +5,17 @@ param(
     $logPath = "$PSScriptRoot\logs\log.txt",
 
     ####### sln details
-    [string] $pathToSln = 'C:\Users\shlomiz\source\repos\Spitfire\Branches\NugetDev',
-    [string] $slnName = 'Client.sln',
+    [string] $pathToSln, #example = 'C:\Users\shlomiz\source\repos\Branches\NugetDev',
+    [string[]] $slnNames, #example = @('PCCQCS.sln','IntegrationTests.sln','ApplicationInsightsDispatcher.sln','SMFMonitor.sln','CommonInfrastructure.sln','Server.sln','OCController.sln','DPControllerCommon.sln','DPController.sln','QTController.sln','Client.sln','PressDataCollector.sln','DFEMock.sln','MigrationWizard.sln'),
     [string] $tfPath = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\TF.exe',
 
     ####### nuget details
-    [string[]] $sources = @('ExternalPackages','MarketPackages'),
+    [string[]] $sources = @('ExternalPackages','MarketPackages','DeletedPackages'),
     $packageProvider = 'NuGet',
     $nugetPath = "C:\Users\shlomiz\source\ConvertingToDependencyManaged\apps\nuget.exe"
 )
 [string[]] $nugetRefs = New-Object -TypeName 'System.Collections.Generic.HashSet[string]'
- $nugetRefs += foreach ($source in $sources){ & $nugetPath list -source $source}
+$nugetRefs += foreach ($source in $sources){ & $nugetPath list -source $source}
 
 ####### 0. functions #######
 <#------------------------#>
@@ -23,10 +23,12 @@ function logThis([string] $message){
     $date = [datetime]::Now
     Out-File -FilePath $logPath -Append -InputObject $date": "$message
 }
+Remove-Item -Path $logPath
 
 ####### 1. get projects list #######
 <#--------------------------------#>
-Remove-Item -Path $logPath
+foreach($slnName in $slnNames){
+
 logThis("scanning $slnName for projects")
 [PSObject[]] $projectsList = & $PSScriptRoot\getSlnProjects.ps1 $pathToSln $slnName
 logThis("found " + $projectsList.Count + " projects")
@@ -34,6 +36,7 @@ logThis("found " + $projectsList.Count + " projects")
 ####### 2. loop projects #######
 <#----------------------------#>
 foreach($project in $projectsList){
+#if($project.Name -eq 'Spitfire.CommonServices.DBF.PDLS.Interfaces.csproj'){
     logThis('      *** project: ' + $project.Name + ' ***' )
     logThis("scanning " + $project.Name + " for refs")
     ####### 2a. get project refs #######
@@ -42,7 +45,7 @@ foreach($project in $projectsList){
 
     ####### 2b. remove system refs #######
     <#----------------------------------#>
-    [string[]] $systemRefs = $refsList | Where-Object {$_.StartsWith('System') -or $_.StartsWith('Microsoft') -or $_.ToLower().Contains('mstsc') -or $_.Contains('PresentationCore') -or $_.Contains('PresentationFramework') -or $_.StartsWith('Windows')}
+    [string[]] $systemRefs = $refsList | Where-Object {$_.StartsWith('System') -or $_.StartsWith('Microsoft') -or $_.ToLower().Contains('mstsc') -or $_.Contains('PresentationCore') -or $_.Contains('PresentationFramework') -or $_.StartsWith('Windows') -or $_.StartsWith('Spitfire') -or $_.StartsWith('Telerik') -or $_.StartsWith('SStuff') -or $_.StartsWith('Interop')}
     foreach($systemRef in $systemRefs){
         $refsList.Remove($systemRef)
     }
@@ -76,11 +79,16 @@ foreach($project in $projectsList){
     ####### 2g. loop refs #######
     <#-------------------------#>
     foreach($ref in $refsList){
+    if($ref -eq 'bitmiracle.libtiff.net' -or $ref -eq 'landaframework.processutilities'){
         logThis("      *** ref: $ref ***")
         ####### 2g1. find ref in feed by name #######
         <#-----------------------------------------#>
         logThis("   searching for " + $ref + " in nuget feed")
-        [bool] $isRefListedInSource = ($nugetRefs -like ("$ref*")).Count -gt 0
+        [bool] $isRefListedInSource = $false
+        if(($nugetRefs -like ("$ref*")).Count -gt 0){ #-or $ref -eq 'CefSharp' -or $ref -eq 'CefSharp.Core'){ 
+            $isRefListedInSource = $true
+        }
+        
         ####### 2g2. Ref Is Listed In Source #######
         <#----------------------------------------#>
         if($isRefListedInSource){
@@ -89,19 +97,34 @@ foreach($project in $projectsList){
             ####### 2g2a. get current xml node by ref name #######
             <#--------------------------------------------------#>
             [System.Xml.XmlLinkedNode[]] $oldNode = $csprojFile.Project.GetElementsByTagName('Reference') | Where-Object ({$_.Include -like "*$ref*"})
-                        
-            ####### 2g2b. recover dll version #######
-            <#-------------------------------------#>
-            if($null -eq $csprojPath + '\..\' + $oldNode.HintPath){
-                logThis("HERE ERROR OF EMPTY DLL PATH: $csprojPath + '\..\' + $oldNode")
+            if($null -eq $oldNode){
+                logThis("HERE ERROR OF EMPTY node for: $ref")
+            }
+            
+            ####### 2g2b. retrieve dll version #######
+            <#--------------------------------------#>
+            [string[]] $hintPath = $oldNode[0].HintPath
+            if($null -eq $oldNode -or $null -eq $csprojPath + '\..\' + $hintPath){
+                logThis("HERE ERROR OF EMPTY DLL PATH for: $ref | $csprojPath + '\..\' + $oldNode")
             }
             else{
-                [string] $dllPath = $csprojPath + '\..\' + $oldNode.HintPath[0]            
+                [string] $dllPath = $csprojPath + '\..\' + $oldNode[0].HintPath
                 $dllVersion = (Get-Item $dllPath).VersionInfo.ProductVersion
-                if($dllVersion -notlike '[0-9].[0-9].[0-9]'){
+                if($dllVersion -notlike '[0-9]*.[0-9]*.[0-9]*' -and $dllVersion -notlike '[0-9]*.[0-9]*.[0-9]*.[0-9]*'){
                     $dllVersion = (Get-Item $dllPath).VersionInfo.FileVersion
-                    if($dllVersion -like '[0-9].[0-9].[0-9].0'){
-                        $dllVersion = $dllVersion.Substring(0,$dllVersion.Length-2)
+                }
+                #if($dllVersion -like '[0-9]*.[0-9]*.[0-9]*.0'){
+                <#if($dllVersion -like '[0-9]*.[0-9]*.[0-9]*.[0-9]*'){
+                    [int] $lastDotIndex = $dllVersion.LastIndexOf('.')
+                    #$dllVersion = $dllVersion.Substring(0,$dllVersion.Length-2)
+                    $dllVersion = $dllVersion.Substring(0,$lastDotIndex)
+                }#>
+                if($null -eq $dllVersion){                    
+                    if($ref -like 'Newtonsoft.Json'){
+                        $dllVersion = '11.0.2.21924'
+                    }
+                    else{
+                        logThis("HERE ERROR OF EMPTY DLL version for: $ref | (Get-Item $dllPath).VersionInfo.ProductVersion")
                     }
                 }
             }
@@ -122,13 +145,27 @@ foreach($project in $projectsList){
             [System.Xml.XmlNodeList] $itemGroups = $csprojFile.Project.GetElementsByTagName('ItemGroup')
             [System.Xml.XmlLinkedNode] $itemGroup = $itemGroups.Item($itemGroups.Count - 1)
             $node = $csprojFile.CreateElement('PackageReference',$csprojFile.Project.NamespaceURI)
+            $versionNode = $csprojFile.CreateElement('Version',$csprojFile.Project.NamespaceURI)
+            $excludeAssetsNode = $csprojFile.CreateElement('ExcludeAssets',$csprojFile.Project.NamespaceURI)
             
             ####### Set Attributes
-            $node.SetAttribute('Include',$ref)
-            $node.SetAttribute('Version',$dllVersion)
+            <#if($ref -eq 'CefSharp' -or $ref -eq 'CefSharp.Core'){
+                $node.SetAttribute('Include','CefSharp.Common')
+                #$node.SetAttribute('Version','73.1.130')
+                $versionNode.InnerText = '73.1.130'
+            }else{#>
+                $node.SetAttribute('Include',$ref)
+                #$node.SetAttribute('Version',$dllVersion)
+                $versionNode.InnerText = $dllVersion                
+                $excludeAssetsNode.InnerText = 'runtime'
+            #}
+            $node.AppendChild($versionNode)
+            $node.AppendChild($excludeAssetsNode)
 
             ####### add child
-            $itemGroup.AppendChild($node)
+            #if(($ref -ne 'CefSharp' -and $ref -ne 'CefSharp.Core') -or $csprojFile.Project.ItemGroup.PackageReference.Where({$_.Include -eq 'CefSharp.Common'})[0] -eq $null){
+                $itemGroup.AppendChild($node)
+            #}
             logThis('   added new ref node for ' + $ref)
         }
         ####### 2g3. Ref Is Not Listed In Source #######
@@ -137,14 +174,15 @@ foreach($project in $projectsList){
             logThis("   ERROR: can't find $ref in nuget sources")
         }
     }
-
+    }
     ####### 2h. save csproj file #######
     <#--------------------------------#>
     logThis("saving csproj file for " + $project.Name)
     $csprojFile.Save($csprojPath)
     logThis("saved csproj file for " + $project.Name)
+#}
 }
-
+}
 ####### 3. nuget restore #######
 <#-----------------------------#>
 logThis('restoring nuget packages so that pending changes will apear in VS2017')
@@ -167,7 +205,7 @@ logThis("$slnName is now dependency-managed")
         # 2g1. find ref in feed by name
         # 2g2. Ref Is Listed In Source
             # 2g2a. get current xml node by ref name
-            # 2g2b. recover dll version
+            # 2g2b. retrieve dll version
             # 2g2c. remove current xml node
             # 2g2d. add new xml entriy to csproj
         # 2g3. Ref Is Not Listed In Source    
